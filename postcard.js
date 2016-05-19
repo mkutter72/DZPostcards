58d987b23e4689e2ba4141792f42f21adcfa291c
MikeKutter <mkutter@yahoo.com>
2016-05-19 11:26:19 -0400

    Update

---
+++ postcard.js
@@ -0,0 +1,67 @@
+'use strict';
+
+$(document).ready(function () {
+
+  if (location.search.indexOf ("url") < 0){
+    // if no URL was input don't activate anything
+    return;
+  }
+
+  var   imgURL = "https://" + location.search.replace("?url=","");
+
+   $('#postCardImage').attr("src", imgURL);
+
+
+  $('#facebookUpload').on('click',function (e){
+    e.preventDefault();
+       var fbURL = "http://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(imgURL);
+
+       window.location.href = fbURL;
+    $('#facebookUpload').blur();
+  });
+
+
+  $('#twitterUpload').on('click',function (e){
+    e.preventDefault();
+
+    OAuth.initialize('3TIpo07N1KfQTivxD61KRgKlft4');
+
+    OAuth.popup('twitter').done(function(twitter) {
+      //make API calls with `twitter`
+       twitter.post('https://upload.twitter.com/1.1/media/upload.json', {
+          data: {
+             media: imageData
+             }
+           }).done(function(data) {
+              //todo with data
+              var media_str = data["media_id_string"];
+
+              twitter.post('/1.1/statuses/update.json', {
+                data: {
+                        status: "My visit to the Detroit Zoo",
+                        media_ids: media_str
+                      }
+                  }).done(function(data) {
+                        alert("Postcard upload to Twitter complete");
+                  }).fail(function(err) {
+                      var errorTxt = JSON.stringify(err, null, 2)
+                      console.log("Error\n" + errorTxt);
+                      alert("Upload failed");
+                  });
+
+            }).fail(function(err) {
+            //todo with err
+              var errorTxt = JSON.stringify(err, null, 2)
+              console.log("Error\n" + errorTxt);
+              alert("Upload failed");
+          });
+
+    }).fail(function(err) {
+        //todo when the OAuth flow failed
+        console.log("popup failed:   "+ err);
+        alert("Upload failed");
+    });
+
+    $('#twitterUpload').blur();
+  });
+});
